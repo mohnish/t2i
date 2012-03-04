0617ddee071ef0fcfb61f2829c0a45a97da019f6
Mohnish Thallavajhula <arrowgunz@gmail.com>
2012-03-03 23:34:26 -0800

    zzzz

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $('#input-text').on('input propertychange', function() {
     var canvas = document.getElementById('output');
-    var img = canvas.toDataURL("image/png");
+    var img = canvas.toDataURL('image/png');
     var width = null;
     if (canvas && canvas.getContext) {
       var context = canvas.getContext('2d');
       if (context) {
         context.fillStyle = '#000';
-        context.font = 'italic 30px sans-serif';
+        context.font = '10pt Arial';
         context.textBaseline = 'top';
         width = context.measureText($('#input-text').val()).width;
         context.fillText($('#input-text').val(), 0, 0);
