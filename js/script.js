4eb20a4e26b527cb49c47f4d8b05adfb03c31e7e
Mohnish Thallavajhula <mohnish@billfloat.com>
2012-06-02 13:18:33 -0700

    removed modernizr and added custom canvas test and removed unnecessary css

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@
   website: http://mohni.sh
 */
 
+$(function() {
+  var input = document.getElementById('input-text')
+    , output = document.getElementById('output-image')
+    , elem = document.createElement('canvas')
+    , canvasTest = false;
 
+  canvasTest = !!(elem.getContext && elem.getContext('2d'));
+
+  if(canvasTest && (typeof document.createElement('canvas').getContext('2d').fillText === 'function')) {
+
+  }
+
+});
 
 
 
