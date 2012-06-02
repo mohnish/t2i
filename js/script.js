6adb20c6a44faa7e3c8e32e5f4943ebe3934ae26
Mohnish Thallavajhula <mohnish@billfloat.com>
2012-06-02 13:24:15 -0700

    removed unncessary styles and jQuery

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@
 $(function() {
   var input = document.getElementById('input-text')
     , output = document.getElementById('output-image')
+    , notice = document.getElementById('notice')
     , elem = document.createElement('canvas')
     , canvasTest = false;
 
-  canvasTest = !!(elem.getContext && elem.getContext('2d'));
+  canvasTest = !!(elem.getContext && elem.getContext('2d') && typeof elem.getContext('2d').fillText === 'function');
 
-  if(canvasTest && (typeof document.createElement('canvas').getContext('2d').fillText === 'function')) {
-
+  if(canvasTest) {
+    notice.css
   }
 
 });
