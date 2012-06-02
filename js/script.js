6b02986e2c1c79e89d303688522c58bc3f5de686
Mohnish Thallavajhula <mohnish@billfloat.com>
2012-06-02 14:17:33 -0700

    data url wip

--- js/script.js
+++ js/script.js
@@ -9,14 +9,19 @@ window.onload = function() {
     , elem = document.createElement('canvas')
     , canvas = document.getElementById('display-text')
     , context = canvas.getContext('2d')
-    , canvasTest = false;
+    , canvasTest = false
+    , imageUrl;
+
 
   canvasTest = !!(elem.getContext && elem.getContext('2d') && typeof elem.getContext('2d').fillText === 'function');
 
   if(canvasTest) {
     // T2I
-    canvas.addEventListener('input', function() {
-      context.fillText(input.value, 100, 30);
+    input.addEventListener('input', function(event) {
+      context.clearRect(0, 0, 500, 90);
+      context.fillText(input.value, 10, 10);
+      imageUrl = canvas.toDataUrl('image/png');
+      output.src = imageUrl;
     });
   } else {
     // Display the notice
