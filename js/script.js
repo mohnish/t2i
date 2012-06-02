b1ca9a06fe86dadc65f758c4e6dec737b94c571c
Mohnish Thallavajhula <mohnish@billfloat.com>
2012-06-02 13:49:31 -0700

    added the input event listener and refactored code

--- js/script.js
+++ js/script.js
@@ -7,14 +7,20 @@ window.onload = function() {
     , output = document.getElementById('output-image')
     , notice = document.getElementById('notice')
     , elem = document.createElement('canvas')
+    , canvas = document.getElementById('display-text')
+    , context = canvas.getContext('2d')
     , canvasTest = false;
 
   canvasTest = !!(elem.getContext && elem.getContext('2d') && typeof elem.getContext('2d').fillText === 'function');
 
   if(canvasTest) {
-    //
+    // T2I
+    canvas.addEventListener('input', function() {
+      context.fillText(input.value, 100, 30);
+    });
   } else {
-    notice.className = '';
+    // Display the notice
+    notice.removeAttribute('class');
   }
 
 };
