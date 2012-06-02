20cbe846dd8fc67f7cf70e91201515c869322696
Mohnish Thallavajhula <mohnish@billfloat.com>
2012-06-02 12:25:20 -0700

    second life

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
-$(document).ready(function() {
-  $('#input-text').on('input propertychange', function() {
-    var canvas = document.getElementById('output');
-    var img = canvas.toDataURL('image/png');
-    var width = null;
-    if (canvas && canvas.getContext) {
-      var context = canvas.getContext('2d');
-      if (context) {
-        context.fillStyle = '#000';
-        context.font = '10pt Arial';
-        context.textBaseline = 'top';
-        width = context.measureText($('#input-text').val()).width;
-        context.fillText($('#input-text').val(), 0, 0);
-      }
-    }
-    $("#output").attr({'src': img, 'width': width});
-  });
-});
+/* Author: Mohnish Thallavajhula
+
+*/
+
+
+
+
+
