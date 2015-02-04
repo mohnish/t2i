027c57984ee7a0b3d4b66ff0320fa2011e309df8
Mohnish Thallavajhula <i@mohni.sh>
2015-02-04 12:40:20 -0800

    remove unnecessary coercion

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
     , elem = document.createElement('canvas')
     , context = canvas.getContext('2d');
 
-  if( !!(elem.getContext && elem.getContext('2d') && typeof elem.getContext('2d').fillText === 'function') ) {
+  if(elem.getContext && elem.getContext('2d') && typeof elem.getContext('2d').fillText == 'function') {
     input.addEventListener('input', function(event) {
       context.clearRect(0, 0, canvas.width, canvas.height);
       context.font = "italic 18pt Calibri";
